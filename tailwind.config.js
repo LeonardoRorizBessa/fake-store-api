d03a060a018ce2801b1ff39c1d8c287850517161
LeonardoRorizBessa <leonardo.rorizbessa@gmail.com>
2025-02-20 11:52:13 -0300

    feat add grid and category filter

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        cinza: '#1c1c1c',
+      }
+    },
+  },
+  plugins: [],
+}
